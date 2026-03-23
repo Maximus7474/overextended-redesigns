f7fc41df37f982e038c341f107838b8256e1d4c6
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-23 13:19:04 +0100

    feat: IamLation ox target redesign

--- src/data/ox_target.ts
+++ src/data/ox_target.ts
@@ -7,5 +7,19 @@ export default {
 	icon: Eye,
 	description: "A Standalone \"third-eye\" targeting resource.",
 	githubUrl: 'https://github.com/CommunityOx/ox_target',
-	redesigns: []
+	redesigns: [
+		{
+			id: "iamlation-target-redesign",
+			name: "Modern UI target",
+			screenshot: "/redesigns/ox_target/iamlation-target-redesign/main_screenshot.webp",
+			description: "A modern UI feel to ox_target",
+			features: [ "Modern UI" ],
+			requirements: [],
+			installationGuide: [
+				"Clone the repository from GitHub or Download latest release"
+			],
+			githubUrl: "https://github.com/IamLation/ox_target",
+			gallery: [],
+		},
+	]
 } satisfies Resource;
