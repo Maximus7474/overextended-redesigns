eebbf838a6b57796c7642c3a9bcad393d645f1df
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-19 12:46:48 +0200

    chore(data/ox_lib): reindent file

--- src/data/ox_lib.ts
+++ src/data/ox_lib.ts
@@ -9,22 +9,22 @@ export default {
 	githubUrl: 'https://github.com/CommunityOx/ox_lib',
 	redesigns: [
 		{
-					id: "rome-lib-redesign",
-					name: "Lib Redesign",
-					screenshot: "/redesigns/ox_lib/rome-lib-redesign/main_screenshot.webp",
-					description: "A modern lib visual redesign",
-					flags: ['gpl-compliant', 'unsupported', 'unmaintained'],
-					features: [],
-					requirements: [],
-					installationGuide: [
-						"Clone the repository from GitHub or Download latest release",
-						"If cloning the repository, build the ui using Bun"
-					],
-					githubUrl: "https://github.com/Scorpion7162/ox_lib_rome_reskin",
-					gallery: [
-						"/redesigns/ox_lib/rome-lib-redesign/image_1.webp",
-						"/redesigns/ox_lib/rome-lib-redesign/image_2.webp",
-					],
-				},
+			id: "rome-lib-redesign",
+			name: "Lib Redesign",
+			screenshot: "/redesigns/ox_lib/rome-lib-redesign/main_screenshot.webp",
+			description: "A modern lib visual redesign",
+			flags: ['gpl-compliant', 'unsupported', 'unmaintained'],
+			features: [],
+			requirements: [],
+			installationGuide: [
+				"Clone the repository from GitHub or Download latest release",
+				"If cloning the repository, build the ui using Bun"
+			],
+			githubUrl: "https://github.com/Scorpion7162/ox_lib_rome_reskin",
+			gallery: [
+				"/redesigns/ox_lib/rome-lib-redesign/image_1.webp",
+				"/redesigns/ox_lib/rome-lib-redesign/image_2.webp",
+			],
+		},
 	]
 } satisfies Resource;
